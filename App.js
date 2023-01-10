a85d506cc6fcd824b7b30fd8b26b42b5c137b7c9
Rahul Rajput <rahulrajput21112@gmail.com>
2023-01-10 17:55:57 +0530

    dotenv

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+
+const { NAME } = Constants.manifest?.extra;
 
 export default function App() {
+  console.log(NAME)
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
